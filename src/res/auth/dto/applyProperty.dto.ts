dd3c89b993243076f0b907a9ef3c773d7d4b0c60
Juan Lee <ninejuan@protonmail.com>
2024-10-07 11:47:43 +0000

    add: applyProperty - JACode, keyword

---
+++ src/res/auth/dto/applyProperty.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
+export class ApplyProperty {
+    @ApiProperty({
+        description: "JACode - 복지정보 지원자격요건입니다.",
+        example: "[\"복지\"]"
+    })
+    jacode: Array<String>;
+
+    @ApiProperty({
+        description: "키워드 - 관심 키워드입니다.",
+        example: "[\"저소득\"]"
+    })
+    keyword: Array<String>;
+};
