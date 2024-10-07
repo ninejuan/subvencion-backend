18077f141a7bce1b3743d8a6377fed277d088f56
Juan Lee <ninejuan@protonmail.com>
2024-10-07 11:36:32 +0000

    update: user interface, schema

--- src/interface/user.interface.ts
+++ src/interface/user.interface.ts
@@ -2,10 +2,9 @@ interface User {
     google_mail: String;
     google_uid: String;
     name?: String;
-    description?: String;
-    associated?: String;
     profilePhoto?: String;
-    refreshToken?: String;
+    jacode: Array<String>;
+    keywords: Array<String>;
 };
 
 export default User;
