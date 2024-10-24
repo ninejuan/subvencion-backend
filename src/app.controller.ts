7fa261527b7cef86195eaf024096d6ae17c30fac
Juan Lee <ninejuan@protonmail.com>
2024-10-25 02:37:45 +0900

    add: main <- getInterestedSubsidies, newSubsidies

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SubJwtAuthGuard } from './res/common/guards/subjwt.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('main/keyword')
-  async getKeyword() {
-    return this.appService.getKeyword();
+  @UseGuards(SubJwtAuthGuard)
+  @Get('main/interested')
+  async getKeyword(@Req() req) {
+    return this.appService.getInterestedSubsidies(req.id, 6);
   }
 
   @Get('main/new')
   async getNew() {
-    return this.appService.getNew();
+    return this.appService.getNew(6);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import subsidySchema from "./models/subsidy.schema";
+import userSchema from "./models/user.schema";
 
 @Injectable()
 export class AppService {
-  async getKeyword() {
-    return process.env.CB_URL;
+  private async getRandomSubsidies(limit: number) {
+    const subsidies = await subsidySchema.aggregate([{ $sample: { size: 6 } }]);
+    return subsidies;
+  }
+  private async getKeywordBasedSubsidies(keywords: string[], limit: number) {
+    const subsidies = await subsidySchema
+      .find({ keywords: { $in: keywords } })
+      .limit(limit);
+    return subsidies;
   }
 
-  async getNew() {
+  async getInterestedSubsidies(userId: string, limit: number) {
+    if (!userId) return await this.getRandomSubsidies(limit);
+    const user = await userSchema.findOne({ google_uid: userId });
+    if (!user.keywords || user.keywords.length == 0)
+      return await this.getRandomSubsidies(limit);
+    return await this.getKeywordBasedSubsidies(user.keywords, limit);
+  }
 
+  async getNew(limit: number) {
+    const subsidies = await subsidySchema.find({}).limit(limit);
+    return subsidies;
   }
 }
